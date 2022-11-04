1826e201c73d776f5b549652aa6de753d5a6061b
Ayodele David <dayodele89@outlook.com>
2022-11-04 15:13:02 +0100

    Added CaptureFrame model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+def upload_capture(instance,filename):
+    return f'frame_capture/{filename}'
+
+
+class CaptureFrame(models.Model):
+    image=models.ImageField(upload_to=upload_capture)
+    created_at=models.DateTimeField(auto_now_add=True)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
+from rest_framework.parsers import MultiPartParser,FormParser
 # Create your views here.
