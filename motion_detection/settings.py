08e810322bc63edb7bda65bfaec291ecb9237aaf
Ayodele David <dayodele89@outlook.com>
2022-11-06 23:48:40 +0100

    update

--- motion_detection/settings.py
+++ motion_detection/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT=Path(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[Path(BASE_DIR,'static')]
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=Path(BASE_DIR,'media')
