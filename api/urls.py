1f78ba59850b00dc2e113b3393ad782adba516c4
Ayodele David <dayodele89@outlook.com>
2022-11-13 23:39:47 +0100

    update

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import AddCapturedFrameView,GetAllCaptures
-
-urlpatterns = [
-    path('capture-frame/',AddCapturedFrameView.as_view(),name='capture-frame'),
-    path('all-captures/',GetAllCaptures.as_view(),name='all-captures')
-]
--- client.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url='http://127.0.0.1:8000/api/all-captures/'
-file={'captured_frame':open("C:/Users/Dayod/OneDrive/Pictures/my_pic.jpg",'rb')}
-response=requests.get(url)
-print(response.json())
